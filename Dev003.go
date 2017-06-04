768049db677e6dcb36e608791a8f2772767fc80d
HajYok <hajime.yokota@tcs.com>
2017-06-05 01:02:18 +0900

    Update Dev003.go

--- Dev003.go
+++ Dev003.go
@@ -40,6 +40,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "write2" {
+		return t.write2(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
@@ -53,6 +55,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Handle different functions
 	if function == "read" { //read a variable
 		return t.read(stub, args)
+	} else if function == "read2" {
+		return t.read2(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
